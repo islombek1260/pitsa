6bc0b4dee863c522d87defeaf348a26cf7414691
islombek1260 <islombekr70@gmail.com>
2022-11-04 03:47:58 +0500

    second-commit

--- js/main.js
+++ js/main.js
@@ -1,88 +1,142 @@
+// "use strict";
+
+
+// ======================================================================================
+// ===================== OBYEKT PITSA ====================================================
+
 var pitsa = new Object()
     hamirTuri = undefined;
     kottaligi = undefined;
-    ustiga = undefined;
+    ustiga = undefined; 
     qoshimchalar = undefined;
 
+// ======================================================================================
+// ===================== TOGGLE MENU ====================================================
     
+let elMenuBtn = document.querySelector(".menu-btn")
+let elSiteNav = document.querySelector(".site-nav__list")
 
-function getValue() {
-    var select = $("#hamirTuri");
-    var value = select.options[select.selectedIndex].value;
-    pitsa.hamirTuri = value;
-    if(value=='yupqa'){
-        $('.yupqa').classList.add('active');
-        $('.qalin').classList.remove('active');
-        $('.hot-dog').classList.remove('active');
-    }else if(value=='qalin'){
-        $('.yupqa').classList.remove('active');
-        $('.qalin').classList.add('active');
-        $('.hot-dog').classList.remove('active');
-    }else{
-        $('.yupqa').classList.remove('active');
-        $('.qalin').classList.remove('active');
-        $('.hot-dog').classList.add('active');
+elMenuBtn.addEventListener('click', function(){
+    elSiteNav.classList.toggle("active")
+})
+
+// =====================================================================================
+// ==================== HAMIR TURI =====================================================
+
+$(".form-select").addEventListener("change", (e) => {
+    e.target.value;
+    $(".check_input_label").addEventListener
+    $(".yupqa").innerHTML = e.target.value;
+    pitsa.hamirTuri = e.target.value;
+})
+
+// =====================================================================================
+// ===================== KOTTALIGI =====================================================
+
+$('#kottaliglari').addEventListener("change", (e) =>{
+    e.target.value; 
+    $(".sm25").innerHTML = e.target.value;
+    pitsa.kottaligi = e.target.value;
+})
+
+// =====================================================================================
+// ===================== USTIGA ========================================================
+
+function e1(){
+    var checkbox = $('.ustiga');
+    var checkboxChecked = [];
+    for (var index = 0; index < checkbox.length; index++) {
+        if (checkbox[index].checked) {
+            checkboxChecked.push(checkbox[index].value);
+            $(".pomidor").innerHTML = checkboxChecked; 
+        }
+    }
+    pitsa.ustiga = checkboxChecked;
+}
+
+// =====================================================================================
+// ===================== QO'SGIMCHA ====================================================
+
+function e2(){
+    var checkbox = $('.qoshimchalar');
+    var checkboxChecked = [];
+    for (var index = 0; index < checkbox.length; index++) {
+        if (checkbox[index].checked) {
+            checkboxChecked.push(checkbox[index].value);   
+        }
+    }
+    pitsa.qoshimchalar = checkboxChecked;
+    $(".achchiq").innerHTML = pitsa.qoshimchalar;
+}
+
+// ======================================================================================
+// ===================== BUYURTMA BERISH ================================================
+
+function zakaz(){
+    switch(undefined){
+        case pitsa.hamirTuri:
+            $('.modal-body').innerHTML=`<span class="pi">Ma'lumot yetarli emas!!!</span>
+            <img class="modal-img" src="../images/images.png" alt="warning">`;
+            break;
+        case pitsa.kottaligi:
+            $('.modal-body').innerHTML=`<span class="pi">Ma'lumot yetarli emas!!!</span>
+            <img class="modal-img" src="../images/images.png" alt="warning">`;
+            break
+        case pitsa.ustiga:
+            $('.modal-body').innerHTML=`<span class="pi">Ma'lumot yetarli emas!!!</span>
+            <img class="modal-img" src="../images/images.png" alt="warning">`;
+            break;
+        case pitsa.qoshimchalar:
+            $('.modal-body').innerHTML=`<span class="pi">Ma'lumot yetarli emas!!!</span>
+            <img class="modal-img" src="../images/images.png" alt="warning">`;
+            break;
+        default:
+            $('.modal-body').innerHTML=`<span class="pi">Buyurtmangiz qabul qilindi</span>
+            <img class="modal-img" src="../images/checked.png" alt="sucses">`;
     }
 
+}
 
-    var kottaliglari = $('input[name="kottaligi"]:checked').value;
-    pitsa.kottaligi = kottaliglari;
-    if(kottaliglari=='25sm'){
-        $('.sm25').classList.add('active');
-        $('.sm30').classList.remove('active');
-        $('.sm35').classList.remove('active');
-    }else if(kottaliglari=='30sm'){
-        $('.sm25').classList.remove('active');
-        $('.sm30').classList.add('active');
-        $('.sm35').classList.remove('active');
-    }else{
-        $('.sm25').classList.remove('active');
-        $('.sm30').classList.remove('active');
-        $('.sm35').classList.add('active');
-        console.log(kottaliglari)
-    }
+// ======================================================================================
+// ===================== MODAL OYNA =====================================================
 
+// Get modal element
+var modal = $('#simpleModal');
+// Get open modal button
+var modalBtn = $('#modalBtn');
+// Get close modal button
+var closeBtn = $('#closeBtn');
 
-    pitsa.ustiga = $$$('ustiga__input');
-    if(pitsa.ustiga=='Pomidor'){
-        $('.pomidor').classList.add('active');
-    }else{
-        $('.pomidor').classList.remove('active');
-    }
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
 
-    if(pitsa.ustiga=='Kurka goshti'){
-        $('.kurka-gushti').classList.add('active');
-    }else{
-        $('.kurka-gushti').classList.remove('active');
-    }
+// Function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
 
-    if(pitsa.ustiga=='zaytun'){
-        $('.zaytun').classList.add('active');
-    }else{
-        $('.zaytun').classList.remove('active');
-    }
+// Function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
 
-    if(pitsa.ustiga=='Tuzlangan bodring'){
-        $('.tuzlangan-bodring').classList.add('active');
-    }else{
-        $('.tuzlangan-bodring').classList.remove('active');
-    }
-    
-    if(pitsa.ustiga=='Qoziqorin'){
-        $('.qoziqorin').classList.add('active');
-    }else{
-        $('.qoziqorin').classList.remove('active');
-    }
-    
-    if(pitsa.ustiga=='Qazi'){
-        $('.qazi').classList.add('active');
-    }else{
-        $('.qazi').classList.remove('active');
+// Function to close modal if outside click
+function outsideClick(e){
+    if(e.target == modal){
+    modal.style.display = 'none';
     }
 }
 
-    pitsa.qoshimchalar = $$$('qoshimcha__input');
-    console,console.log(pitsa);
+
+
+
+
+
+
 
 
 
